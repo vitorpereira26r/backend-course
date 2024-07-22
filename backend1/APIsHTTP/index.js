c187cfbd9a48bfa466173705f8b0c6f131be4aaf
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2024-07-22 15:48:38 -0300

    HTTP module

--- backend1/APIsHTTP/index.js
+++ backend1/APIsHTTP/index.js
@@ -1,1 +1,22 @@
 const http = require('http')
+
+const PORT = 4000;
+
+const server = http.createServer((req, res) => {
+    console.log("In the API")
+    console.log(req.method)
+    console.log(req.url)
+
+    if(req.method === 'GET' && req.url.startsWith('/users')){
+        res.writeHead(200);
+        res.end('all users');
+        return;
+    }
+
+    res.writeHead(400)
+    res.end("Resource not found")
+})
+
+server.listen(PORT, () => {
+    console.log("Server started on port " + PORT)
+})
