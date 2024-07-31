2d7926ed4de96dbee19cedcf902a409c4eeab9dc
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2024-07-31 10:18:35 -0300

    updating user

--- backend2/classOne/index.js
+++ backend2/classOne/index.js
@@ -9,6 +9,7 @@ async function connectAndSync() {
         await User.sync();
         console.log("User model synchronized successfully!");
 
+        // INSERT
         const newUser = await User.create({
             name: "Vitor",
             email: "vitorpereira@gmail.com",
@@ -16,8 +17,16 @@ async function connectAndSync() {
         });
         console.log("User created successfully.");
 
+        // SELECT
         const users = await User.findAll();
         console.log("User's list: ", users.map((u) => u.toJSON()));
+
+        // UPDATE
+        const user = await User.findByPk(newUser.id);
+        await user.update({ password: "newpassword" });
+        console.log("User updated successfully: ", user.toJSON());
+
+        //
     } catch (error) {
         console.log("Error connecting to the database", error);
     } finally {
